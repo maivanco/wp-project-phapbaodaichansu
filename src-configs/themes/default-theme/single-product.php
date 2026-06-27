4a58656d083056cd6cfa5aa118d4cd35afdfc260
Mike.personal <thanhhaitran93@gmail.com>
2026-06-27 15:18:55 +0700

    add plugins

---
+++ src-configs/themes/default-theme/single-product.php
@@ -0,0 +1,29 @@
+<?php
+get_header();
+
+/* Start the Loop */
+while (have_posts()) :
+	the_post();
+
+?>
+
+<section class="breadcrumbs py-4">
+	<div class="container">
+		<?php
+		if (function_exists('yoast_breadcrumb')) {
+			yoast_breadcrumb();
+		}
+		?>
+	</div>
+</section>
+
+<div class="page-wrapper">
+	<div class="container
+		after:content-[''] after:block after:clear-both">
+			<?php the_content(); ?>
+		</div>
+	</div>
+<?php
+endwhile; // End of the loop.
+
+get_footer();
--- src-configs/themes/default-theme/single.php
+++ src-configs/themes/default-theme/single.php
@@ -6,41 +6,22 @@ while (have_posts()) :
 	the_post();
 
 ?>
-	<div class="page-wrapper sec has-pb">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-9 col-12">
-					<div class="hero">
-						<h1 class="text-center post-title"><?= get_the_title() ?></h1>
-						<div class="detail d-flex justify-content-center align-items-center">
-							<p class="date"><?= get_the_date() ?></p>
-							<i class="fas fa-circle"></i>
-							<div class="categories"><?php the_category(','); ?></div>
-							<i class="fas fa-circle"></i>
-							<p class=""><?= get_the_author_meta('display_name') ?></p>
-						</div>
-					</div>
-					<div id="page-detail">
 
-						<?php the_content(); ?>
+<section class="breadcrumbs py-4">
+	<div class="container">
+		<?php
+		if (function_exists('yoast_breadcrumb')) {
+			yoast_breadcrumb();
+		}
+		?>
+	</div>
+</section>
 
-						<div class="relative-tags">
-							<ul class="list">
-								<?php
-								$tags = get_the_tags();
-								if (!empty($tags)) {
-									foreach ($tags as $tag) {
-										$tag_link = get_tag_link($tag->term_id);
-										echo '<li class="item"><a href="' . esc_url($tag_link) . '" title="' . esc_attr($tag->name) . '" class="tag-link-' . esc_attr($tag->term_id) . '">' . esc_html($tag->name) . '</a></li>';
-									}
-								}
-								?>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<?= get_sidebar() ?>
-			</div>
+<div class="page-wrapper">
+	<div class="container
+		after:content-[''] after:block after:clear-both">
+			<?php the_title();?>
+			<?php the_content(); ?>
 		</div>
 	</div>
 <?php
