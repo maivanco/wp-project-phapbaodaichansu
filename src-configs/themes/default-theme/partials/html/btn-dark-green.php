9d0a3d3ce784507f1edad364a0ceeb7a9e95a4f7
Mike.personal <thanhhaitran93@gmail.com>
2026-06-28 14:17:49 +0700

    update btn  + define agents file

--- src-configs/themes/default-theme/partials/html/btn-dark-green.php
+++ src-configs/themes/default-theme/partials/html/btn-dark-green.php
@@ -1,16 +1,26 @@
 <?php 
 $btn_label = $args['label'] ?? '';
-$btn_url = $args['url'] ?? '#';
+$btn_href = $args['href'] ?? '#';
 $btn_class = 'bg-[#1a747a] border-1 text-[#fff] leading-[18px] py-[15px] rounded-[30px] text-center';
 $target = $args['target'] ?? '_self';
+$custom_attrs = $args['custom_attrs'] ?? [];
+
+$custom_attr_str = '';
+
+if (!empty($custom_attrs)) {
+    foreach($custom_attrs as $attr => $value){
+        $custom_attr_str .= ' ' . $attr . '="' . $value . '"';
+    }
+}
 
 if(!empty($args['class'])) {
     $btn_class .= ' ' . $args['class'];
 }
+
 $btn_class = explode(' ', $btn_class);
 $btn_class = array_unique($btn_class);
 $btn_class = implode(' ', $btn_class);
 ?>
-<a href="<?php echo $btn_url ?>" class="<?php echo $btn_class ?>" target="<?php echo $target ?>">
+<a href="<?php echo $btn_href ?>" class="<?php echo $btn_class ?>" target="<?php echo $target ?>" <?php echo $custom_attr_str ?>>
     <?php echo $btn_label ?>
 </a>
